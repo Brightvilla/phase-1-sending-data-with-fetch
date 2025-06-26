40a948ba08e705e0498c7b67d9c32d12161f9c78
Brightvilla <166039620+Brightvilla@users.noreply.github.com>
2025-06-26 12:10:24 +0300

    complete sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+// index.js
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += `<p>${data.id}</p>`;
+      return data;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
+
+window.submitData = submitData;
+
+
+
+
+
+
